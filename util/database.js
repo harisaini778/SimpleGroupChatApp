5fdaf46f3d8ac45d86316d55edd99beaae9d87bb
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-12-03 13:45:49 +0530

    added sequelize to the project

--- util/database.js
+++ util/database.js
@@ -1,14 +1,22 @@
 // /util/database.js
 
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password : "Sql123"
-})
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password : "Sql123"
+// })
 
-module.exports = pool;
+// module.exports = pool;
+
+const Sequelize = require('sequelize');
 
 
+const sequelize = new Sequelize('node-complete', 'roor', 'Sql123', {
+    dialect : 'mysql',
+    host : 'localhost',
+});
+
+module.exports = sequelize;
